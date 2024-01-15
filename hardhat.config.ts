74883206ffedac5b3ec888554891f1dce449d2ee
David Yaari <david,yaari@icloud.com>
2024-01-15 09:28:35 +0200

    commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
+import dotenv from 'dotenv';
+import '@nomiclabs/hardhat-ethers';
 
-const config: HardhatUserConfig = {
+dotenv.config();
+
+module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: '0.8.16',
     settings: {
+      viaIR: true,
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 100,
       },
     },
   },
+  networks: {
+    sepolia: {
+      url: `${process.env.SEPOLIA_URL}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
-
-export default config;
